f4d6cdd30c4673ed45960f36e8d200fe4b8c0d0b
gatbysolid <56706560+gatbysolid@users.noreply.github.com>
2020-01-30 14:21:21 +0900

    testupload

--- finalproject/src/main/java/com/kh/finalproject/controller/QuestionController.java
+++ finalproject/src/main/java/com/kh/finalproject/controller/QuestionController.java
@@ -1,5 +1,14 @@
 package com.kh.finalproject.controller;
 
-public class QuestionController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/question")
+public class QuestionController {
+	@GetMapping("/upload")
+	public String upload() {
+		return "question/upload";
+	}
 }
