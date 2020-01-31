f09f253fc8442b17d3285fcb4cd786a5e3b7bdbf
irekizea <56664373+irekizea@users.noreply.github.com>
2020-01-31 19:11:55 +0900

    문제 체점 완료

--- finalproject/src/main/java/com/kh/finalproject/controller/SolveQuestionController.java
+++ finalproject/src/main/java/com/kh/finalproject/controller/SolveQuestionController.java
@@ -4,10 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.kh.finalproject.entity.TestDto;
 import com.kh.finalproject.repository.TestDao;
 
 import lombok.extern.slf4j.Slf4j;
@@ -38,5 +37,6 @@ public class SolveQuestionController {
 		return "question/plural";
 		
 	}
+
 	
 }
