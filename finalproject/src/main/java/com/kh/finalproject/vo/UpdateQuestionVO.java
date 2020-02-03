7bdbd704a332c8fdab21793aad380af3e4571e8e
gatbysolid <56706560+gatbysolid@users.noreply.github.com>
2020-02-03 10:28:33 +0900

    파일내 폰트깨짐 수정

--- finalproject/src/main/java/com/kh/finalproject/vo/UpdateQuestionVO.java
+++ finalproject/src/main/java/com/kh/finalproject/vo/UpdateQuestionVO.java
@@ -11,8 +11,7 @@ import lombok.NoArgsConstructor;
 
 @Data@NoArgsConstructor@AllArgsConstructor@Builder
 public class UpdateQuestionVO {
-	//문제 업로드
-	private int question_no;//하기 2 테이블에 외래키로 묶여 있음.
+	private int question_no;
 	private int user_custom_question_no; 
 	private String question_title; 
 	private String question_content; 
@@ -25,14 +24,13 @@ public class UpdateQuestionVO {
 	private String answer4;
 	private String answer5;
 /*************************************************/
-	//user_costom_question
-	private int user_no;//유저 넘버
+	private int user_no;
 	private String category_name;
 /*************************************************/
-	private int question_file_no;//번호(기본키)
-	private String file_upload_name;//올린이름
-	private String file_save_name;//실제이름
-	private String file_type;//파일유형
-	private long file_size;//파일크기
+	private int question_file_no;
+	private String file_upload_name;
+	private String file_save_name;
+	private String file_type;
+	private long file_size;
 	private List<MultipartFile> file;
 }
