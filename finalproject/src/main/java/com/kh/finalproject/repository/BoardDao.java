218bdbae402f1bb7f9b07cc59424d0345fd964c1
user1 <user1@DESKTOP-0IFG55Q>
2020-02-04 18:24:41 +0900

    게시판 파일업로드, 게시글 수정 구현

--- finalproject/src/main/java/com/kh/finalproject/repository/BoardDao.java
+++ finalproject/src/main/java/com/kh/finalproject/repository/BoardDao.java
@@ -1,8 +1,12 @@
 package com.kh.finalproject.repository;
 
+import java.io.IOException;
 import java.util.List;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import com.kh.finalproject.entity.BoardDto;
+import com.kh.finalproject.entity.BoardFileDto;
 
 public interface BoardDao {
 	//1. 등록기능
@@ -16,4 +20,12 @@ public interface BoardDao {
 
 	//4.카테고리별 게시판 조회기능
 		List<BoardDto> getCategoryList(String board_category);
+		
+	//5.게시글 번호 자동 발급	
+		int getsequence();
+		
+	//6. 게시글 수정
+		void edit(BoardDto boardDto);
+		
+		
 }
